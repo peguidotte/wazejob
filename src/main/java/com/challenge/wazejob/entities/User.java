f607caf501e1050d6d154417941e3bd89ece838d
Pedro Guidotte <peguidotte@gmail.com>
2025-11-14 20:57:05 -0300

    wip

--- src/main/java/com/challenge/wazejob/entities/User.java
+++ src/main/java/com/challenge/wazejob/entities/User.java
@@ -1,7 +1,38 @@
 package com.challenge.wazejob.entities;
 
-import jakarta.persistence.Entity;
+import com.challenge.wazejob.enums.UserRole;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class User {
+
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String userId;
+
+    @NotBlank
+    @Size(max = 100)
+    @Schema(description = "User's full (or not) name", example = "Pedro Guidotte")
+    private String name;
+
+    @NotBlank
+    @Email
+    @Size(max = 100)
+    @Column(unique = true)
+    @Schema(description = "User's email address", example = "peguidotte@gmail.com")
+    private String email;
+
+    @NotBlank
+    @Size(min = 7)
+    @Schema(description = "A strong password please", example = "strongPassword123")
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Schema(description = "Your GitHub profile URL", example = "https://github.com/peguidotte")
+    private String github;
 }
---
+++ src/main/java/com/challenge/wazejob/enums/UserRole.java
@@ -0,0 +1,6 @@
+package com.challenge.wazejob.enums;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
